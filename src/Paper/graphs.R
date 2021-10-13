87e2b490276d6dc082430e4ba94fdd121f4edea5
mirandamanschot <90325075+mirandamanschot@users.noreply.github.com>
2021-10-13 17:22:08 +0200

    Update and rename graphs to graphs.R

---
+++ src/Paper/graphs.R
@@ -0,0 +1,13 @@
+install.packages("ggplot2")
+library(ggplot2)
+
+ggplot(listingssub, aes(priceperson)) + geom_bar()
+ggplot(listingssub, aes(classification_nights, priceperson)) + geom_point() + geom_boxplot()
+ggplot(listingssub, aes(classification_nights)) + geom_bar()
+
+
+ggplot(listingssub, aes(x = classification_nights, y = priceperson, color= classification_nights)) +
+  geom_point() +
+  geom_boxplot() 
+ggplot(listingssub, aes(x = classification_neigbourhood, y = priceperson, color=classification_neigbourhood)) +
+  geom_boxplot()
