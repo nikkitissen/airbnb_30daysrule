b9ca45de2546febd6aa283a4dcdaad7c9badb829
LotteVerkuijlen <l.verkuijlen@tilburguniversity.edu>
2021-10-14 16:13:44 +0200

    update download_data

--- src/preparation/download_data.R
+++ src/preparation/download_data.R
@@ -1,11 +1,15 @@
 ######################
 ### DOWNLOAD DATA ####
 ######################
-download_data <- function(url, filename){
-  download.file(url = url, destfile = paste0(filename, ".csv"))
+# To download the data programmatically from the web; downloads a file from a URL and stores it on your local machine. 
+download_data <- function(url, filename, filepath){
+  dir.create(filepath)
+  download.file(url = url, destfile = paste0(filepath,filename, ".csv"))
 }
 
+# Downloading the raw listings data for the city Amsterdam
 url_listings <- "http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2021-09-07/data/listings.csv.gz"
 
-download_data(url_listings, "listings")
+download_data(url_listings, "listings", 'data/')
+
 
