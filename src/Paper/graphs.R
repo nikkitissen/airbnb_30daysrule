8ce990dcffc0752dfe2f466eb00d5f51f1b8511f
mirandamanschot <90325075+mirandamanschot@users.noreply.github.com>
2021-10-16 18:00:21 +0200

    Update graphs.R

--- src/Paper/graphs.R
+++ src/Paper/graphs.R
@@ -8,6 +8,7 @@ library(ggplot2)
 listingssub <- read.csv("gen/temp/listingssub.csv")
 View(listingssub)
 
+#create graphs of priceperson & classification_nights and priceperson & classification_neighbourhood. Save both in one file
 pdf("gen/output/graphs_airbnb.pdf")
 
 ggplot(listingssub, aes(x = classification_nights, y = priceperson, color= classification_nights)) +
