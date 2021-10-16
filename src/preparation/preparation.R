0874b7dda4ad62bd3ccdf3e5b54c4725517645e0
mirandamanschot <90325075+mirandamanschot@users.noreply.github.com>
2021-10-16 17:57:23 +0200

    Update preparation.R

--- src/preparation/preparation.R
+++ src/preparation/preparation.R
@@ -19,7 +19,7 @@ listings$priceperson= (listings$price_1/accomodates_num)
 #neighbourhoods + frequency0
 table(listings$neighbourhood_cleansed)
 
-#dummy sticks to 30days
+#3 dummies for the 30 day rules
 listings$dummy30days <- ifelse(listings$maximum_nights <= 30, 1, 0)
 listings$dummy3060days <- ifelse(listings$maximum_nights >= 31 & listings$maximum_nights <= 60, 1, 0)
 listings$dummy61days <- ifelse(listings$maximum_nights >= 61, 1, 0)
@@ -35,10 +35,12 @@ listings$dummyCentrum <-ifelse(listings$neighbourhood_cleansed =="Centrum-Oost"|
 listings$dummyCentrumOost <- ifelse(listings$neighbourhood_cleansed =="Centrum-Oost", 1, 0)
 listings$dummyCentrumWest <- ifelse(listings$neighbourhood_cleansed =="Centrum-West", 1, 0)
 
+# delete columns that are not needed and filter such that the maximum night is 90. The 90 ensures equal sized treatment groups.
 listingssub <- listings[c(1:6,9:13,18,21,23,28,33,34,36:42,75:83)]
 listingssub <- listingssub%>%filter(maximum_nights <= 90)
 
-#classification 30/60/90 days
+#classification 30/60/90 days such that the anova is run at once.
+#firt create a new empty variable, then assign the values 0,1,2 to them in accordance with the dummy groups
 listingssub$classification_nights <- ''
 listingssub$classification_nights <- ifelse(listingssub$maximum_nights<=30, '0',listingssub$classification_nights)
 listingssub$classification_nights <- ifelse(listingssub$maximum_nights>30&listingssub$maximum_nights<=60, '1',listingssub$classification_nights)
@@ -48,6 +50,7 @@ summary(aov(priceperson~1+classification_nights, data = listingssub))
 table(listingssub$classification_nights)
 
 #classification for neighbourhoods
+#firt create a new empty variable, then assign the values 0 and 1 to them in accordance with the dummy groups
 listingssub$classification_neigbourhood <- ''
 listingssub$classification_neigbourhood <- ifelse(listingssub$neighbourhood_cleansed =="Noord-Oost", '0', listingssub$classification_neigbourhood)
 listingssub$classification_neigbourhood <- ifelse(listingssub$neighbourhood_cleansed =="Noord-West", '0', listingssub$classification_neigbourhood)
