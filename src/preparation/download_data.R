33d56cdda90d4bb04d32bc126f6b46765a3840e5
LotteVerkuijlen <l.verkuijlen@tilburguniversity.edu>
2021-10-18 12:27:58 +0200

    add directory

--- src/preparation/download_data.R
+++ src/preparation/download_data.R
@@ -1,6 +1,10 @@
 ######################
 ### DOWNLOAD DATA ####
 ######################
+
+# create dictionary
+dir.create(("data"), showWarnings = FALSE)
+
 # To download the data programmatically from the web; downloads a file from a URL and stores it on your local machine. 
 download_data <- function(url, filename, filepath){
   dir.create(filepath)
@@ -10,6 +14,10 @@ download_data <- function(url, filename, filepath){
 # Downloading the raw listings data for the city Amsterdam
 url_listings <- "http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2021-09-07/data/listings.csv.gz"
 
+# store data in data folder
 download_data(url_listings, "listings", 'data/')
+
+
+
 
 
