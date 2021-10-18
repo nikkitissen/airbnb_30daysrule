018feed932103f4724cbf401bb2dc6f3e622dbf0
LotteVerkuijlen <90325046+LotteVerkuijlen@users.noreply.github.com>
2021-10-18 16:03:55 +0200

    Update analysis.R

--- src/analysis/analysis.R
+++ src/analysis/analysis.R
@@ -9,8 +9,11 @@ library(car)
 listingssub <- read.csv("gen/temp/listingssub.csv")
 View(listingssub)
 
-#Run multiple ANOVAs for the classifications + interaction and for the dummies
+# Make the classification variables factor variables
+listingssub$classification_neigbourhood <- as.factor(listingssub$classification_neigbourhood)
+listingssub$classification_nights <- as.factor(listingssub$classification_nights)
 
+# Run multiple ANOVAs for the classifications + interaction and for the dummies
 Priceperson_aov1 <- aov(priceperson ~ classification_nights + classification_neigbourhood, listingssub)
 summary(Priceperson_aov1)
 Priceperson_aov2 <- aov(priceperson ~ classification_nights + classification_neigbourhood + classification_neigbourhood:classification_nights, listingssub)
@@ -20,14 +23,14 @@ summary(Priceperson_aov3)
 Priceperson_aov4 <- aov(priceperson ~ dummy3060days + dummy61days, listingssub)
 summary(Priceperson_aov4)
 
-#ANOVA assumptions + save graphs as PDF
+# ANOVA assumptions + save graphs as PDF
 pdf("gen/output/assumptions.pdf")
 plot(Priceperson_aov1, 1)  #homogeneity
 leveneTest(priceperson ~ classification_neigbourhood*classification_nights, listingssub) 
 plot(Priceperson_aov2, 2) #normality
 dev.off()
 
-#save output ANOVA
+# save output ANOVA
 capture.output(summary(Priceperson_aov1), file = "gen/output/anova1.doc")
 capture.output(summary(Priceperson_aov2), file = "gen/output/anova2.doc")
 capture.output(summary(Priceperson_aov3), file = "gen/output/anova3.doc")
